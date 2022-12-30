24fc12c628bfb0c87868ec3aa41eedadaf4a8d84
Dmitry <dmitrychuiko03@gmail.com>
2022-12-30 13:58:07 +0200

    Added Entities to DAL
    Added Entities to DAL

---
+++ system-of-energy-consumers/DAL/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    internal class User
+    {
+        public int id { get; set; }
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string patronimyc { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public string IEnumerable<House> Houses { get; set; }
+
+
+    }
+}
